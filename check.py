486d6239147cfeec19426bc25f90f9b12a9b5ad2
zihan liao <71817562+ZihanLiao-ZJU@users.noreply.github.com>
2022-11-17 15:50:26 +0800

    Update check.py

--- check.py
+++ check.py
@@ -281,7 +281,7 @@ if __name__ == '__main__':
     reminders = os.getenv("REMINDERS")
     lng= os.getenv("lng") # 经度
     lat= os.getenv("lat") # 维度
-    user = [0,2,3,4,5]
+    user = [2,3,4,5,0]
     Nuser = len(user);
     for iuser in range(Nuser):
         username = os.getenv("account{}".format(user[iuser]))
