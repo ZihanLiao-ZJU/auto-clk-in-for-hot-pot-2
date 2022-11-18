2b714e31de9a6d1b662a4022d48f2b2c067b0167
zihan liao <71817562+ZihanLiao-ZJU@users.noreply.github.com>
2022-11-18 16:02:39 +0800

    Update check.py

--- check.py
+++ check.py
@@ -282,6 +282,7 @@ if __name__ == '__main__':
     lng= os.getenv("lng") # 经度
     lat= os.getenv("lat") # 维度
     user = [0,2,3,4,5]
+    Nuser = len(user)
     for iuser in range(Nuser):
         username = os.getenv("account{}".format(user[iuser]))
         password = os.getenv("password{}".format(user[iuser]))
